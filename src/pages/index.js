6a69e460ecf3c238618e78565a588353cfd66860
Isaac Sokari <isaacsokari@gmail.com>
2020-11-24 19:00:37 +0100

    added blog-post template, modified gatsby-node api to use markdown files to create pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <SEO title={`Home`} />
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <>
+        <h2>
+          <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+        </h2>
+        {node.frontmatter.description ? (
+          <small>{node.frontmatter.description} -</small>
+        ) : null}
+        <small>{node.frontmatter.date}</small>
+        <p>{node.excerpt}</p>
+      </>
+    ))}
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            description
+            date
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
